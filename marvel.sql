895d45ab7bdae5da34bfd9b5187cf3ecc18885f3
leeburgess67 <leeburgessmail@gmail.com>
2018-06-04 16:21:19 +0100

    10 quesitons complete

--- marvel.sql
+++ marvel.sql
@@ -209,8 +209,55 @@ SELECT name FROM people;
 
 INSERT INTO movies (title, year, show_time) VALUES ('Avengers: Infinity War', 2010, '00:00');
 SELECT * FROM movies;
+-- id |                title                | year | show_time
+-- ----+-------------------------------------+------+-----------
+--  1 | Iron Man                            | 2008 | 23:55
+--  2 | The Incredible Hulk                 | 2008 | 12:10
+--  3 | Iron Man 2                          | 2010 | 15:45
+--  4 | Thor                                | 2011 | 16:25
+--  5 | Captain America: The First Avenger  | 2011 | 16:55
+--  6 | Avengers Assemble                   | 2012 | 13:25
+--  7 | Iron Man 3                          | 2013 | 23:15
+--  8 | Thor: The Dark World                | 2013 | 16:50
+-- 10 | Captain America: The Winter Soldier | 2014 | 14:55
+-- 11 | Guardians of the Galaxy             | 2014 | 19:05
+-- 12 | Avengers: Age of Ultron             | 2015 | 17:45
+-- 13 | Ant-Man                             | 2015 | 16:30
+-- 14 | Captain America: Civil War          | 2016 | 16:00
+-- 15 | Doctor Strange                      | 2016 | 16:10
+-- 16 | Guardians of the Galaxy 2           | 2017 | 17:25
+-- 17 | Spider-Man: Homecoming              | 2017 | 13:50
+-- 18 | Thor: Ragnarok                      | 2017 | 14:40
+-- 19 | Black Panther                       | 2018 | 15:00
+-- 20 | Avengers: Infinity War              | 2010 | 00:00
 --
 -- 9.  The cinema would also like to make the Guardians movies a back to back feature. Find out the show time of "Guardians of the Galaxy" and set the show time of "Guardians of the Galaxy 2" to start two hours later.
+
+UPDATE movies SET show_time = '21:05' WHERE id = 16;
+SELECT * FROM movies;
+
+-- id |                title                | year | show_time 
+-- ----+-------------------------------------+------+-----------
+--  1 | Iron Man                            | 2008 | 23:55
+--  2 | The Incredible Hulk                 | 2008 | 12:10
+--  3 | Iron Man 2                          | 2010 | 15:45
+--  4 | Thor                                | 2011 | 16:25
+--  5 | Captain America: The First Avenger  | 2011 | 16:55
+--  6 | Avengers Assemble                   | 2012 | 13:25
+--  7 | Iron Man 3                          | 2013 | 23:15
+--  8 | Thor: The Dark World                | 2013 | 16:50
+-- 10 | Captain America: The Winter Soldier | 2014 | 14:55
+-- 11 | Guardians of the Galaxy             | 2014 | 19:05
+-- 12 | Avengers: Age of Ultron             | 2015 | 17:45
+-- 13 | Ant-Man                             | 2015 | 16:30
+-- 14 | Captain America: Civil War          | 2016 | 16:00
+-- 15 | Doctor Strange                      | 2016 | 16:10
+-- 17 | Spider-Man: Homecoming              | 2017 | 13:50
+-- 18 | Thor: Ragnarok                      | 2017 | 14:40
+-- 19 | Black Panther                       | 2018 | 15:00
+-- 20 | Avengers: Infinity War              | 2010 | 00:00
+-- 16 | Guardians of the Galaxy 2           | 2017 | 21:05
+
 --
 -- ## Extension
 --
